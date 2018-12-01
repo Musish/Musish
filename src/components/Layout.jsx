1756c1062333bbb45a820231e4dcc2cfca4985ba
Brychan Odlum <brychan@me.com>
2018-12-01 11:24:34 -0800

    Core layout implementation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 export default class Layout extends React.Component {
   render() {
-    return this.props.children;
+    return (
+      <Fragment>
+
+        <aside id="main-sidebar">
+          <div class="menu">
+            <ul>
+              <li class="active">
+                <a href="#">Library</a>
+              </li>
+              <li>
+                <a href="#">Playlists</a>
+              </li>
+              <li>
+                <a href="#">Artists</a>
+              </li>
+              <li>
+                <a href="#">Albums</a>
+              </li>
+              <li>
+                <a href="#">Songs</a>
+              </li>
+            </ul>
+          </div>
+        </aside>
+
+        <main id="main-content">
+          { this.props.children }
+        </main>
+      </Fragment>
+    );
   }
 }
