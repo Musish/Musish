05afe8c33b6f84ec46968b3a3f0d6d935e89f2c2
Raphael Vigee <contact@raphaelvigee.com>
2018-12-01 10:57:38 -0800

    Add router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import MusicKitProvider from './MusicKitProvider';
 import MusicKitAuthorizeProvider from './MusicKitAuthorizeProvider';
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 
 export default class App extends React.Component {
   render() {
     return (
         <MusicKitProvider>
           <MusicKitAuthorizeProvider>
-            Listen to some music *yay*
+            <Router>
+              <Switch>
+                <Route path="/" exact component={() => "Home"}/>
+                <Route path="/test1" exact component={() => "Test 1"}/>
+                <Route path="/test2" exact component={() => "Test 2"}/>
+                <Redirect to="/"/>
+              </Switch>
+            </Router>
           </MusicKitAuthorizeProvider>
         </MusicKitProvider>
     )
