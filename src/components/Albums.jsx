7833b4b19b3274c9a849347e2e6f70d4d9d61836
Raphael Vigee <contact@raphaelvigee.com>
2018-12-01 11:01:46 -0800

    Add layout & albums

---
+++ src/components/Albums.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Albums extends React.Component {
+  render() {
+    return "Albums"
+  }
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import MusicKitProvider from './MusicKitProvider';
 import MusicKitAuthorizeProvider from './MusicKitAuthorizeProvider';
-import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Albums from './Albums';
+import Layout from './Layout';
 
 export default class App extends React.Component {
   render() {
     return (
         <MusicKitProvider>
           <MusicKitAuthorizeProvider>
-            <Router>
-              <Switch>
-                <Route path="/" exact component={() => "Home"}/>
-                <Route path="/test1" exact component={() => "Test 1"}/>
-                <Route path="/test2" exact component={() => "Test 2"}/>
-                <Redirect to="/"/>
-              </Switch>
-            </Router>
+            <Layout>
+              <Router>
+                <Switch>
+                  <Route path="/" exact component={() => 'Home'}/>
+                  <Route path="/albums" exact component={Albums}/>
+                  <Redirect to="/"/>
+                </Switch>
+              </Router>
+            </Layout>
           </MusicKitAuthorizeProvider>
         </MusicKitProvider>
-    )
+    );
   }
 }
