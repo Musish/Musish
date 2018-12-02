2fbafd233ed364e58aac658712e8c00d2b93b39e
Raphael Vigee <contact@raphaelvigee.com>
2018-12-01 18:47:16 -0800

    Fix playlist reload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,7 +28,7 @@ export default class App extends React.Component {
                   <Route path="/albums" exact component={Albums}/>
                   <Route path="/albums/:id" exact component={Album}/>
                   <Route path="/playlists" exact component={Playlists}/>
-                  <Route path="/playlists/:id" exact component={Playlist}/>
+                  <Route path="/playlists/:id" exact component={props => <Playlist key={props.location.pathname} {...props} />} />
                   <Route path="/artists" exact component={Artists}/>
                   <Route path="/artists/:id" exact component={Artist}/>
                   <Route path="/songs" exact component={Songs}/>
