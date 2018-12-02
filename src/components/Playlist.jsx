78a959b6faa197c5e09db4a2a237935cc52f4b8f
Raphael Vigee <contact@raphaelvigee.com>
2018-12-01 18:01:59 -0800

    FIX FILIP

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,46 +1,35 @@
 import React from 'react';
-import MusicKitProvider from './MusicKitProvider';
-import MusicKitAuthorizeProvider from './MusicKitAuthorizeProvider';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch, withRouter,
-} from 'react-router-dom';
-import Albums from './Albums';
-import AlbumItem from './AlbumItem';
-import Layout from './Layout';
-import Artists from './Artists';
-import Artist from './Artist';
+import {withRouter} from 'react-router-dom';
 
 class Playlist extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      playlist: null
-    }
+      playlist: null,
+    };
   }
 
   async componentDidMount() {
     const music = MusicKit.getInstance();
-    const playlist = await music.api.library.playlist(this.props.match.params.id)
+    const playlist = await music.api.library.playlist(
+        this.props.match.params.id);
 
     console.log(playlist);
 
     this.setState({
-      playlist
-    })
+      playlist,
+    });
   }
 
   render() {
-    if(!this.state.playlist) {
-      return "Loading..."
+    if (!this.state.playlist) {
+      return 'Loading...';
     }
     return (
-      <div>
-        {this.state.playlist.id}
-      </div>
+        <div>
+          {this.state.playlist.id}
+        </div>
     );
   }
 }
