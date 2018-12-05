a57ad551dc2be6f3bf035fb5b2dc419f406bf80e
Brychan Odlum <brychan@me.com>
2018-12-04 20:14:33 -0800

    A little less potential trademark infringement

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,10 +5,10 @@ import LoginScss from './Login.scss';
 export default class Login extends Component {
   render() {
     return (
-        <div className={LoginScss.buttonContainer}>
-          <h1 className={LoginScss.title}> Musi.sh</h1>
-          <h5 className={LoginScss.subheading}>Please connect to start browsing through your music.</h5>
-          <button type={"button"} onClick={this.props.onClick} className={LoginScss.button}>Connect to  Music</button>
+        <div className={LoginScss.loginContainer}>
+          <h1 className={LoginScss.title}>Musi.sh</h1>
+          <h5 className={LoginScss.subheading}>Just like <i className="fab fa-apple" /> Music... ish.</h5>
+          <button type={"button"} onClick={this.props.onClick} className={LoginScss.button}>Connect to Apple</button>
         </div>
     );
   }
