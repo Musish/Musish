b49ad618561db30ab1ce99bb94cd48d29219c831
Brychan Odlum <brychan@me.com>
2018-12-04 20:51:50 -0800

    Apple style albums

--- src/components/Core/Albums/AlbumItem.jsx
+++ src/components/Core/Albums/AlbumItem.jsx
@@ -1,21 +1,21 @@
 import React, {Component} from 'react';
 
-import AlbumScss from './AlbumItem.scss';
+import Classes from './AlbumItem.scss';
 import {Link} from 'react-router-dom';
 
 export default class AlbumItem extends Component {
   render() {
     return (
-        <Link to={`/albums/${this.props.id}`} className={AlbumScss.container}>
-          <div className={AlbumScss.imageContainer}>
+        <Link to={`/albums/${this.props.id}`} className={Classes.container}>
+          <div className={Classes.imageContainer}>
             <img src={this.props.url}
-                 className={AlbumScss.image}
+                 className={Classes.image}
                  alt={'image'}/>
           </div>
 
-          <div className={AlbumScss.descriptionContainer}>
-            <h3>{this.props.title}</h3>
-            <h4>{this.props.name}</h4>
+          <div className={Classes.descriptionContainer}>
+            <span className={Classes.albumName}>{this.props.title}</span>
+            <span className={Classes.artistName}>{this.props.name}</span>
           </div>
         </Link>
     );
