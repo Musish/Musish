400d7b45c32ef0019268f284a516a5a0f2c38eff
Brychan Odlum <brychan@me.com>
2018-12-14 21:36:08 -0800

    Fixing #9: Removing partial global styles, also removing options from top navigation bar to instead use sidebar.

--- src/components/Core/Layout/NavigationBar/NavigationBar.jsx
+++ src/components/Core/Layout/NavigationBar/NavigationBar.jsx
@@ -7,18 +7,6 @@ export default class NavigationBar extends React.Component {
         <div className="nav-wrapper">
           <h1 className="brand"> Musi.sh</h1>
           <ul className="nav-menu">
-            <li className="active">
-              <a href="#">Library</a>
-            </li>
-            <li className="">
-              <a href="#">Discover</a>
-            </li>
-            <li className="">
-              <a href="#">Social</a>
-            </li>
-            <li className="">
-              <a href="#">Radio</a>
-            </li>
           </ul>
           <div className="nav-search">
             <input type="text" placeholder="Search  Music" />
--- src/components/Core/Layout/Sidebar/Sidebar.jsx
+++ src/components/Core/Layout/Sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import cx from 'classnames';
 import {Link, Route} from 'react-router-dom';
 import Player from '../../Player/Player';
+import classes from './Sidebar.scss';
 
 export default class Sidebar extends React.Component {
 
@@ -32,9 +33,9 @@ export default class Sidebar extends React.Component {
     ));
 
     return (
-      <aside id="main-sidebar">
-        <div className="menus">
-          <div className="menu library">
+      <aside className={classes.sidebar}>
+        <div className={classes.menus}>
+          <div className={classes.menu}>
             <h3>My Library</h3>
             <ul>
               <MenuItem to={'/artists'} label={'Artists'}/>
@@ -42,8 +43,16 @@ export default class Sidebar extends React.Component {
               <MenuItem to={'/songs'} label={'Songs'}/>
             </ul>
           </div>
+          <div className={classes.menu}>
+            <h3>Apple Music</h3>
+            <ul>
+              <MenuItem to={'/discover'} label={'Discover'}/>
+              <MenuItem to={'/foryou'} label={'For You'}/>
+              <MenuItem to={'/radio'} label={'Radio'}/>
+            </ul>
+          </div>
           {playlists && (
-            <div className="menu playlists">
+            <div className={classes.menu}>
               <h3>Playlists</h3>
               <ul>
                 {playlists}
@@ -61,7 +70,7 @@ class MenuItem extends React.Component {
   render() {
     return (
       <Route path={this.props.to} exact children={({match}) => (
-        <li className={cx({active: !!match})}>
+        <li className={!!match ? classes.active : ''}>
           <Link to={this.props.to}>
             {this.props.label}
           </Link>
