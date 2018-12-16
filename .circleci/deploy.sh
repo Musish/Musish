f24186a5dff8897d411d5eab1f92b14ee62848ad
CI/CD <cicd@musi.sh>
2018-12-16 04:21:09 +0000

    Automated deployment to GitHub Pages: 03e7b207185336a5c1631b6778256150918e0333

---
+++ .circleci/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+git config --global user.email $GH_EMAIL
+git config --global user.name $GH_NAME
+
+git clone $CIRCLE_REPOSITORY_URL out
+
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cp ./CNAME /tmp/CNAME || echo "No CNAME file"
+git rm -rf .
+cp /tmp/CNAME ./CNAME || echo "No CNAME file"
+cd ..
+
+yarn build
+
+cp -a build/. out/.
+
+mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
+cd out
+
+./.circleci/generate_structure.sh
+
+git add -A
+git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+
+git push origin $TARGET_BRANCH
+cd ..
