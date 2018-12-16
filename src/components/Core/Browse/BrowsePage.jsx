1d33ee20d5a885c8f631d7b0a1da64087161d576
Brychan Odlum <brychan@me.com>
2018-12-16 02:36:37 -0800

    Browse page

--- src/components/Core/Browse/BrowsePage.jsx
+++ src/components/Core/Browse/BrowsePage.jsx
@@ -4,14 +4,16 @@ import PageContent from "../Layout/PageContent";
 import PageTitle from "../../common/PageTitle";
 import classes from "./BrowsePage.scss";
 import Loader from "../../common/Loader";
+import AlbumItem from "../Albums/AlbumItem";
+import PlaylistItem from "../Playlists/PlaylistItem";
+import SongList from "../common/SongList/SongList";
 
 class BrowsePage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      frequentlyPlayed: null,
-      recentlyPlayed: null,
+      charts: null,
     };
 
     this.ref = React.createRef();
@@ -19,28 +21,54 @@ class BrowsePage extends React.Component {
 
   async componentDidMount() {
     const music = MusicKit.getInstance();
-    let frequentlyPlayed = music.api.historyHeavyRotation();
-    let recentlyPlayed = music.api.recentPlayed();
+    const charts = await music.api.charts(['songs', 'albums', 'playlists'], {limit: 40} );
 
-    frequentlyPlayed = await frequentlyPlayed;
-    recentlyPlayed = await recentlyPlayed;
+    console.log(charts);
 
     this.setState({
-      frequentlyPlayed,
-      recentlyPlayed
+      charts,
     })
   }
 
   render() {
-    const {frequentlyPlayed, recentlyPlayed} = this.state;
+    const {charts} = this.state;
+
     return (
       <PageContent innerRef={this.ref}>
         <PageTitle
           title={"Browse"}
           context={"Apple Music"}
         />
-        <h3>Frequently played</h3>
-        <Loader/>
+        <h3>Hot playlists</h3>
+        <div className={classes.scrollWrapper}>
+          <div className={classes.scrollGrid}>
+            {charts && charts.playlists[0].data.map((playlist, i) => {
+              return (
+                <PlaylistItem key={i} playlist={playlist} size={100}/>
+              );
+            })}
+          </div>
+        </div>
+        <h3>Popular albums</h3>
+        <div className={classes.scrollWrapper}>
+          <div className={classes.scrollGrid}>
+            {charts && charts.albums[0].data.map((album, i) => {
+              return (
+                <AlbumItem key={i} album={album} size={100}/>
+              );
+            })}
+          </div>
+        </div>
+        <h3>Top songs</h3>
+        <div className={classes.chartingSongs}>
+          {charts && (
+            <SongList
+              scrollElement={this.ref}
+              load={() => charts.songs[0].data.slice(0, 10)}
+              showArtist={true}
+            />
+          )}
+        </div>
       </PageContent>
     );
   }
