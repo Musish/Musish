842e21964fc3c0c39d796a781d0355c78389ad5b
fgrebows <fgrebows@cisco.com>
2018-12-17 00:22:43 -0800

    Added a handleSearch function upon sarch input click.

--- src/components/Core/Layout/NavigationBar/NavigationBar.jsx
+++ src/components/Core/Layout/NavigationBar/NavigationBar.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import css from './NavigationBar.scss';
 
 export default class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch() {
+
+  }
+
   render() {
     return (
       <nav className={css.navigationBar}>
         <div className={css.navWrapper}>
           <h1 className={css.brand}> Musi.sh</h1>
           <div className={css.navSearch}>
-            <input className={css.searchInput} type="text" placeholder="Search music" />
+            <input className={css.searchInput} type="text" placeholder="Search Music" onClick={this.handleSearch}/>
           </div>
         </div>
       </nav>
