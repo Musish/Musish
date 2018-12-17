7d60b27bb9dc8286e4b298fb29eed4245d07ef66
fgrebows <fgrebows@cisco.com>
2018-12-17 00:18:44 -0800

    Changed css naming.

--- src/components/Core/Layout/NavigationBar/NavigationBar.jsx
+++ src/components/Core/Layout/NavigationBar/NavigationBar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import classes from './NavigationBar.scss';
+import css from './NavigationBar.scss';
 
 export default class NavigationBar extends React.Component {
   render() {
     return (
-      <nav className={classes.navigationBar}>
-        <div className={classes.navWrapper}>
-          <h1 className={classes.brand}> Musi.sh</h1>
-          <div className={classes.navSearch}>
-            <input type="text" placeholder="Search music" />
+      <nav className={css.navigationBar}>
+        <div className={css.navWrapper}>
+          <h1 className={css.brand}> Musi.sh</h1>
+          <div className={css.navSearch}>
+            <input className={css.searchInput} type="text" placeholder="Search music" />
           </div>
         </div>
       </nav>
