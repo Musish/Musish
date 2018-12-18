a33f330e06147e9ea1e93a5a6e1b0ce298e7105a
Brychan Odlum <brychan@me.com>
2018-12-17 21:39:39 -0800

    wip

--- src/components/Core/Player/Queue/Queue.jsx
+++ src/components/Core/Player/Queue/Queue.jsx
@@ -2,32 +2,70 @@ import React, {Component} from 'react';
 import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 import {List} from 'react-virtualized';
 import classes from './Queue.scss';
+import withMK from "../../../../hoc/withMK";
+import cx from 'classnames';
+
+const QueueItemState = {
+  Played: 0,
+  Playing: 1,
+  Queued: 2,
+};
 
 const SortableItem = SortableElement(({value, style, item}) => {
+  let stateClass;
+  switch (item.queueState) {
+    case QueueItemState.Played:
+      stateClass = classes.played;
+      break;
+    case QueueItemState.Playing:
+      stateClass = classes.playing;
+      break;
+    default:
+      stateClass = classes.queued;
+      break;
+  }
+
   return (
-    <div className={classes.SortableItem} style={style}>
-      {item.value}
+    <div className={cx(classes.SortableItem, stateClass)} style={style}>
+      <span className={classes.title}>{item.attributes.name}</span>
+      <span className={classes.artistName}>{item.attributes.artistName}</span>
+      <span className={classes.albumName}>{item.attributes.albumName}</span>
+
     </div>
   );
 });
 
 class QueueList extends Component {
+  queueState(index, position) {
+    if (index < position) {
+      return QueueItemState.Played;
+    } else if (index === position) {
+      return QueueItemState.Playing;
+    } else {
+      return QueueItemState.Queued;
+    }
+  }
+
   render() {
-    const {items} = this.props;
+    const queue = this.props.mk.instance.player.queue;
+    const items = queue.items.map((item, index) => {
+      item.queueState = this.queueState(index, queue.position);
+      return item;
+    }).filter(item => item.queueState !== QueueItemState.Played);
 
     return (
       <List
         ref={(instance) => {
           this.List = instance;
         }}
-        rowHeight={40}
+        rowHeight={55}
         rowRenderer={(args) => {
           const {index, key} = args;
           const item = items[index];
           return <SortableItem key={key} item={item} {...args} />;
         }}
         rowCount={items.length}
-        width={250}
+        width={275}
         height={370}
         className={classes.SortableList}
       />
@@ -35,26 +73,20 @@ class QueueList extends Component {
   }
 }
 
-const SortableList = SortableContainer(QueueList, {withRef: true});
+const bindings = {
+  [MusicKit.Events.queueItemForStartPosition]: 'queueItemForStartPosition',
+  [MusicKit.Events.queueItemsDidChange]: 'queueItems',
+  [MusicKit.Events.queuePositionDidChange]: 'queuePosition',
+};
+
+const SortableList = withMK(SortableContainer(QueueList, {withRef: true}), bindings);
 
 export default class Queue extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      items: [
-        {value: 'Item 1'},
-        {value: 'Item 2'},
-        {value: 'Item 3'},
-        {value: 'Item 4'},
-        {value: 'Item 5'},
-        {value: 'Item 6'},
-        {value: 'Item 2'},
-        {value: 'Item 3'},
-        {value: 'Item 4'},
-        {value: 'Item 5'},
-        {value: 'Item 6'},
-      ],
+      items: [],
     };
 
     this.onSortEnd = this.onSortEnd.bind(this);
