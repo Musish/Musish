f9cc26b9d0ad88065149fea44c666f773ee24300
rvigee <contact@raphaelvigee.com>
2018-12-19 21:43:55 -0800

    Style song result

--- src/components/Core/Layout/NavigationBar/Search/SongResultItem.jsx
+++ src/components/Core/Layout/NavigationBar/Search/SongResultItem.jsx
@@ -3,6 +3,7 @@ import cx from "classnames";
 import classes from "./SearchBar.scss";
 import withMK from "../../../../../hoc/withMK";
 import {createMediaItem} from "../../../common/Utils";
+import SongDecoration from "../../../common/SongList/SongDecoration";
 
 function SongResultItem({song, mk}) {
   const play = async () => {
@@ -16,8 +17,15 @@ function SongResultItem({song, mk}) {
   return (
     <div className={cx(classes.result, classes.song)}
          key={song.id}
-         onClick={play}>
-      {song.attributes.name}
+         onClick={play}
+    >
+      <div className={classes.artwork}>
+        <SongDecoration song={song} showAlbum={true} size={30}/>
+      </div>
+
+      <span className={classes.name}>
+        {song.attributes.name}
+      </span>
     </div>
   )
 }
--- src/components/Core/common/SongList/SongListItem.jsx
+++ src/components/Core/common/SongList/SongListItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import {artworkForMediaItem, createMediaItem} from "../Utils";
+import {artworkForMediaItem, createMediaItem, isCurrentItem, isPlaying} from "../Utils";
 import classes from "./SongList.scss";
 import {ContextMenuTrigger} from "react-contextmenu";
 import PropTypes from 'prop-types';
 import {MENU_TYPE} from "./SongList";
 import cx from 'classnames';
 import withMK from "../../../../hoc/withMK";
+import SongDecoration from "./SongDecoration";
 
 function collect(props, {props: song, playSong, queueNext, queueLater, state: {artworkURL}}) {
   return {
@@ -22,12 +23,6 @@ function collect(props, {props: song, playSong, queueNext, queueLater, state: {a
 class SongListItem extends React.Component {
   constructor(props) {
     super(props);
-
-    const artworkURL = artworkForMediaItem(this.props.song, 40);
-
-    this.state = {
-      artworkURL: artworkURL,
-    };
 
     this.playSong = this.playSong.bind(this);
     this.pauseSong = this.pauseSong.bind(this);
@@ -72,47 +67,17 @@ class SongListItem extends React.Component {
     return d.getUTCMinutes() + ':' + String('0' + d.getUTCSeconds()).slice(-2);
   }
 
-  isCurrentItem() {
-    const {song, mk} = this.props;
-    const mediaItem = mk.instance.player && mk.instance.player.nowPlayingItem;
-
-    if (!mediaItem) {
-      return false;
-    }
-
-    return song.id === mediaItem.container.id;
-  }
-
   isPlaying() {
-    return this.isCurrentItem() && this.props.mk.instance.player.isPlaying;
+    const {song} = this.props;
+
+    return isPlaying(song);
   }
 
   renderIcon() {
     const {showAlbum, song} = this.props;
-    const isCurrentItem = this.isCurrentItem();
 
-    const playingAnimation = (
-      <div className={cx(classes.playingAnimation, {[classes.animated]: this.isPlaying()})}>
-        <div><span/><span/><span/><span/><span/></div>
-      </div>
-    );
 
-    return (
-      <>
-        {showAlbum ? (
-          <span className={classes.albumArtwork}>
-            {isCurrentItem && playingAnimation}
-            <span className={classes.artworkWrapper}>
-              <img src={this.state.artworkURL} alt=""/>
-            </span>
-          </span>
-        ) : (
-          <span className={classes.songIndex}>
-            {isCurrentItem ? playingAnimation : song.attributes.trackNumber}
-          </span>
-        )}
-      </>
-    );
+    return <SongDecoration song={song} showAlbum={showAlbum}/>
   }
 
   render() {
