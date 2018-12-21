d591fe65b103fd19df97493f218e474d56bc27ad
fgrebows <fgrebows@cisco.com>
2018-12-20 21:42:47 -0800

    Changes.

--- src/components/Core/Layout/NavigationBar/Logout/Logout.jsx
+++ src/components/Core/Layout/NavigationBar/Logout/Logout.jsx
@@ -4,6 +4,13 @@ import classes from './Logout.scss';
 import withMK from '../../../../../hoc/withMK';
 
 class Logout extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.logout = this.logout.bind(this);
+  }
+
   logout() {
     this.props.mk.instance.unauthorize();
     window.location.reload();
@@ -12,7 +19,7 @@ class Logout extends Component {
   render() {
     return (
       <div className={classes.logoutWrapper}>
-        <span onClick={() => this.logout()}>Logout</span>
+        <span onClick={this.logout}>Logout</span>
       </div>
     );
   }
