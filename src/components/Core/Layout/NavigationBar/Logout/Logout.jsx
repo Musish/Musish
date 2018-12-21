775a43cba6332197e16e982d3168e9201a69a5ca
fgrebows <fgrebows@cisco.com>
2018-12-20 19:06:54 -0800

    Added basic structure of logout.

---
+++ src/components/Core/Layout/NavigationBar/Logout/Logout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Logout.scss';
+
+export default class Logout {
+
+  render() {
+    return(
+      <div className={classes.logoutWrapper}>
+        <span>Logout</span>
+      </div>
+    );
+  }
+}
