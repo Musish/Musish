ac94d06427c64d99a466c6454b91fdb55b19eb40
fgrebows <fgrebows@cisco.com>
2018-12-20 20:58:06 -0800

    Finished doing logout.

--- src/components/Core/Layout/NavigationBar/Logout/Logout.jsx
+++ src/components/Core/Layout/NavigationBar/Logout/Logout.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Logout.scss';
+import withMK from '../../../../../hoc/withMK';
 
-export default class Logout {
+class Logout extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  logout() {
+    this.props.mk.instance.unauthorize();
+    window.location.reload();
+  }
 
   render() {
     return(
       <div className={classes.logoutWrapper}>
-        <span>Logout</span>
+          <span onClick={() => this.logout()}>Logout</span>
       </div>
     );
   }
 }
+
+export default withMK(Logout);
