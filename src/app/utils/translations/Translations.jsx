b6c1bf3ec38976f4e90d331ff08d76524b56294f
fgrebows <fgrebows@cisco.com>
2019-01-25 22:32:31 -0800

    A file storing all the language translations.

---
+++ src/app/utils/translations/Translations.jsx
@@ -0,0 +1,420 @@
+import LocalizedStrings from 'react-localization';
+
+const strings = new LocalizedStrings({
+  // ENGLISH
+  en: {
+    musishDescription:
+      'Musish uses your Apple Music library to provide you with an amazing listening ' +
+      'experience. Please connect your account below.',
+    connect: 'Connect to Apple Music',
+    securityMessage: ' Secure authentication via Apple.com.',
+    justBrowse: 'Or just browse',
+    legalNotice:
+      'Musish is not affiliated with Apple, Inc. Our service does not access, ' +
+      "collect, or store any personal or account information. 'Apple', 'Apple Music' and " +
+      'the Apple logo are trademarks of Apple, Inc.',
+
+    appleMusic: 'Apple Music',
+
+    logout: 'Logout',
+    login: 'Login',
+    searchMusic: 'Search music',
+
+    play: 'Play',
+    shuffle: 'Shuffle',
+
+    forYou: 'For You',
+    recentlyPlayed: 'Recently played',
+    heavyRotation: 'Heavy Rotation',
+
+    browse: 'Browse',
+    topCharts: 'Top Charts',
+    genres: 'Genres',
+    topSongs: 'Top Songs',
+    dailyTop100: 'Daily Top 100',
+    topPlaylists: 'Top Playlists',
+    topAlbums: 'Top Albums',
+
+    radio: 'Radio',
+    radioMessage: `We're working on bringing Radio to you ASAP. Check back later`,
+
+    myLibrary: 'My Library',
+    recentlyAdded: 'Recently Added',
+    artists: 'Artists',
+    albums: 'Albums',
+    songs: 'Songs',
+    playlists: 'Playlists',
+
+    feedback: 'Feedback',
+    feedbackMessageOne: 'Designed with',
+    feedbackMessageTwo: 'by the Musish team.',
+
+    lyrics: 'Lyrics',
+    upNext: 'Up next',
+
+    showCompleteAlbum: 'Show Complete Album',
+    hour: 'hour',
+    hours: 'hours',
+    minute: 'minute',
+    minutes: 'minutes',
+  },
+  // POLISH
+  pl: {
+    musishDescription: '',
+    securityMessage: '',
+    justBrowse: '',
+    legalNotice: '',
+
+    appleMusic: 'Muzyka Apple',
+
+    logout: 'Wyloguj się',
+    login: 'Zaloguj się',
+    searchMusic: '',
+
+    play: '',
+    shuffle: '',
+
+    forYou: '',
+    recentlyPlayed: '',
+    heavyRotation: '',
+
+    browse: '',
+    topCharts: '',
+    genres: '',
+    topSongs: '',
+    dailyTop100: '',
+    topPlaylists: '',
+    topAlbums: '',
+
+    radio: '',
+    radioMessage: '',
+
+    myLibrary: '',
+    recentlyAdded: '',
+    artists: '',
+    albums: '',
+    songs: '',
+    playlists: '',
+
+    feedback: '',
+    feedbackMessageOne: '',
+    feedbackMessageTwo: '',
+
+    lyrics: 'Tekst',
+    upNext: '',
+
+    showCompleteAlbum: '',
+    hour: '',
+    hours: '',
+    minute: '',
+    minutes: '',
+  },
+  // FRENCH
+  fr: {
+    musishDescription: '',
+    securityMessage: '',
+    justBrowse: '',
+    legalNotice: '',
+
+    appleMusic: '',
+
+    logout: '',
+    login: '',
+    searchMusic: '',
+
+    play: '',
+    shuffle: '',
+
+    forYou: '',
+    recentlyPlayed: '',
+    heavyRotation: '',
+
+    browse: '',
+    topCharts: '',
+    genres: '',
+    topSongs: '',
+    dailyTop100: '',
+    topPlaylists: '',
+    topAlbums: '',
+
+    radio: '',
+    radioMessage: '',
+
+    myLibrary: '',
+    recentlyAdded: '',
+    artists: '',
+    albums: '',
+    songs: '',
+    playlists: '',
+
+    feedback: '',
+    feedbackMessageOne: '',
+    feedbackMessageTwo: '',
+
+    lyrics: '',
+    upNext: '',
+
+    showCompleteAlbum: '',
+    hour: '',
+    hours: '',
+    minute: '',
+    minutes: '',
+  },
+  // ITALIAN
+  it: {
+    musishDescription: '',
+    securityMessage: '',
+    justBrowse: '',
+    legalNotice: '',
+
+    appleMusic: '',
+
+    logout: '',
+    login: '',
+    searchMusic: '',
+
+    play: '',
+    shuffle: '',
+
+    forYou: '',
+    recentlyPlayed: '',
+    heavyRotation: '',
+
+    browse: '',
+    topCharts: '',
+    genres: '',
+    topSongs: '',
+    dailyTop100: '',
+    topPlaylists: '',
+    topAlbums: '',
+
+    radio: '',
+    radioMessage: '',
+
+    myLibrary: '',
+    recentlyAdded: '',
+    artists: '',
+    albums: '',
+    songs: '',
+    playlists: '',
+
+    feedback: '',
+    feedbackMessageOne: '',
+    feedbackMessageTwo: '',
+
+    lyrics: '',
+    upNext: '',
+
+    showCompleteAlbum: '',
+    hour: '',
+    hours: '',
+    minute: '',
+    minutes: '',
+  },
+  // LITHUANIAN
+  lt: {
+    musishDescription: '',
+    securityMessage: '',
+    justBrowse: '',
+    legalNotice: '',
+
+    appleMusic: '',
+
+    logout: '',
+    login: '',
+    searchMusic: '',
+
+    play: '',
+    shuffle: '',
+
+    forYou: '',
+    recentlyPlayed: '',
+    heavyRotation: '',
+
+    browse: '',
+    topCharts: '',
+    genres: '',
+    topSongs: '',
+    dailyTop100: '',
+    topPlaylists: '',
+    topAlbums: '',
+
+    radio: '',
+    radioMessage: '',
+
+    myLibrary: '',
+    recentlyAdded: '',
+    artists: '',
+    albums: '',
+    songs: '',
+    playlists: '',
+
+    feedback: '',
+    feedbackMessageOne: '',
+    feedbackMessageTwo: '',
+
+    lyrics: '',
+    upNext: '',
+
+    showCompleteAlbum: '',
+    hour: '',
+    hours: '',
+    minute: '',
+    minutes: '',
+  },
+  // SPANISH
+  es: {
+    musishDescription: '',
+    securityMessage: '',
+    justBrowse: '',
+    legalNotice: '',
+
+    appleMusic: '',
+
+    logout: '',
+    login: '',
+    searchMusic: '',
+
+    play: '',
+    shuffle: '',
+
+    forYou: '',
+    recentlyPlayed: '',
+    heavyRotation: '',
+
+    browse: '',
+    topCharts: '',
+    genres: '',
+    topSongs: '',
+    dailyTop100: '',
+    topPlaylists: '',
+    topAlbums: '',
+
+    radio: '',
+    radioMessage: '',
+
+    myLibrary: '',
+    recentlyAdded: '',
+    artists: '',
+    albums: '',
+    songs: '',
+    playlists: '',
+
+    feedback: '',
+    feedbackMessageOne: '',
+    feedbackMessageTwo: '',
+
+    lyrics: '',
+    upNext: '',
+
+    showCompleteAlbum: '',
+    hour: '',
+    hours: '',
+    minute: '',
+    minutes: '',
+  },
+  // RUSSIAN
+  ru: {
+    musishDescription: '',
+    securityMessage: '',
+    justBrowse: '',
+    legalNotice: '',
+
+    appleMusic: '',
+
+    logout: '',
+    login: '',
+    searchMusic: '',
+
+    play: '',
+    shuffle: '',
+
+    forYou: '',
+    recentlyPlayed: '',
+    heavyRotation: '',
+
+    browse: '',
+    topCharts: '',
+    genres: '',
+    topSongs: '',
+    dailyTop100: '',
+    topPlaylists: '',
+    topAlbums: '',
+
+    radio: '',
+    radioMessage: '',
+
+    myLibrary: '',
+    recentlyAdded: '',
+    artists: '',
+    albums: '',
+    songs: '',
+    playlists: '',
+
+    feedback: '',
+    feedbackMessageOne: '',
+    feedbackMessageTwo: '',
+
+    lyrics: '',
+    upNext: '',
+
+    showCompleteAlbum: '',
+    hour: '',
+    hours: '',
+    minute: '',
+    minutes: '',
+  },
+  // GREEK
+  gr: {
+    musishDescription: '',
+    securityMessage: '',
+    justBrowse: '',
+    legalNotice: '',
+
+    appleMusic: '',
+
+    logout: '',
+    login: '',
+    searchMusic: '',
+
+    play: '',
+    shuffle: '',
+
+    forYou: '',
+    recentlyPlayed: '',
+    heavyRotation: '',
+
+    browse: '',
+    topCharts: '',
+    genres: '',
+    topSongs: '',
+    dailyTop100: '',
+    topPlaylists: '',
+    topAlbums: '',
+
+    radio: '',
+    radioMessage: '',
+
+    myLibrary: '',
+    recentlyAdded: '',
+    artists: '',
+    albums: '',
+    songs: '',
+    playlists: '',
+
+    feedback: '',
+    feedbackMessageOne: '',
+    feedbackMessageTwo: '',
+
+    lyrics: '',
+    upNext: '',
+
+    showCompleteAlbum: '',
+    hour: '',
+    hours: '',
+    minute: '',
+    minutes: '',
+  },
+});
+
+export default strings;
