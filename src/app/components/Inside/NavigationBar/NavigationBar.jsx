ba0bb4bdd37b266629d44823e76bcaa3d3297d9f
Marin Binzari <mbinzari@pentalog.com>
2019-03-14 17:54:18 +0200

    Fix for #312 For you page empty when not logged in and click on musish logo

--- src/app/components/Inside/NavigationBar/NavigationBar.jsx
+++ src/app/components/Inside/NavigationBar/NavigationBar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 import classes from './NavigationBar.scss';
 import SearchBar from './Search/SearchBar';
 import Authorize from './Authorize/Authorize';
 import Settings from './Settings/Settings';
+import AuthorizeContext from './Authorize/AuthorizeContext';
+import withContext from '../../../hoc/withContext';
 
-export default function NavigationBar() {
+function NavigationBar(props) {
   return (
     <nav className={classes.navigationBar}>
       <h1 className={classes.brand}>
-        <Link to="/">
+        <Link to={props.authorized ? '/' : '/browse'}>
           <span className={cx(classes.icon, 'musicon musicon-logo')} />
           <span className={classes.name}>Musish</span>
         </Link>
@@ -22,3 +25,9 @@ export default function NavigationBar() {
     </nav>
   );
 }
+
+NavigationBar.propTypes = {
+  authorized: PropTypes.bool.isRequired,
+};
+
+export default withContext(NavigationBar, AuthorizeContext);
