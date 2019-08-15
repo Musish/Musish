74efeabcd05273dad1fda1e1816365241d430efa
rvigee <contact@raphaelvigee.com>
2019-08-15 11:35:38 +0200

    Move AuthorizeButton to TS

--- src/app/components/Common/NavigationBar/AuthorizationButton/AuthorizationButton.tsx
+++ src/app/components/Common/NavigationBar/AuthorizationButton/AuthorizationButton.tsx
@@ -1,33 +1,31 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classes from './AuthorizationButton.scss';
-import withMK from '../../../../hoc/withMK';
-import { AuthorizeContext } from '../../../Providers/AuthorizeProvider';
 import withContext from '../../../../hoc/withContext';
+import withMK from '../../../../hoc/withMK';
 import translate from '../../../../utils/translations/Translations';
+import { AuthorizeContext } from '../../../Providers/AuthorizeProvider';
+import classes from './AuthorizationButton.scss';
 
-function AuthorizationButton(props) {
+interface IAuthorizationButtonProps extends IMKProps {
+  authorized: boolean;
+}
+
+const AuthorizationButton: React.FC<IAuthorizationButtonProps> = ({ mk, authorized }) => {
   async function logout() {
-    await props.mk.instance.unauthorize();
+    await mk.instance.unauthorize();
   }
 
   async function login() {
-    await props.mk.instance.authorize();
+    await mk.instance.authorize();
     window.location.reload();
   }
 
-  const button = props.authorized ? (
+  const button = authorized ? (
     <span onClick={logout}>{translate.logout}</span>
   ) : (
     <span onClick={login}>{translate.login}</span>
   );
 
   return <div className={classes.authorizeWrapper}>{button}</div>;
-}
-
-AuthorizationButton.propTypes = {
-  mk: PropTypes.any.isRequired,
-  authorized: PropTypes.bool.isRequired,
 };
 
 export default withMK(withContext(AuthorizationButton, AuthorizeContext));
