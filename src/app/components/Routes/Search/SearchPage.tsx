b204275051dfb8ec2b394f9598794442e745a5f8
rvigee <contact@raphaelvigee.com>
2019-08-15 11:55:05 +0200

    Move SearchPage to TS

--- src/app/components/Routes/Search/SearchPage.tsx
+++ src/app/components/Routes/Search/SearchPage.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { RouteComponentProps } from 'react-router';
 import { withRouter } from 'react-router-dom';
-
-import classes from './SearchPage.scss';
 import withMK from '../../../hoc/withMK';
-import PageTitle from '../../Common/PageTitle/PageTitle';
-import PageContent from '../../Common/PageContent/PageContent';
-import Loader from '../../Common/Loader/Loader';
-import AlbumItem from '../../Common/AlbumItem/AlbumItem';
-import PlaylistItem from '../../Common/PlaylistItem/PlaylistItem';
-import TracksGrid from '../../Common/Tracks/TracksGrid/TracksGrid';
 import * as MusicPlayerApi from '../../../services/MusicPlayerApi';
-import ArtistItem from '../../Common/ArtistItem/ArtistItem';
-import Tabs from '../../Common/Tabs/Tabs';
-import Tab from '../../Common/Tabs/Tab';
 import translate from '../../../utils/translations/Translations';
+import AlbumItem from '../../Common/AlbumItem/AlbumItem';
+import ArtistItem from '../../Common/ArtistItem/ArtistItem';
+import Loader from '../../Common/Loader/Loader';
+import PageContent from '../../Common/PageContent/PageContent';
+import PageTitle from '../../Common/PageTitle/PageTitle';
+import PlaylistItem from '../../Common/PlaylistItem/PlaylistItem';
+import Tab from '../../Common/Tabs/Tab';
+import Tabs from '../../Common/Tabs/Tabs';
+import TracksGrid from '../../Common/Tracks/TracksGrid/TracksGrid';
+import classes from './SearchPage.scss';
 
-class SearchPage extends React.Component {
-  constructor(props) {
+interface ISearchPageProps
+  extends IMKProps,
+    RouteComponentProps<{ query: string; source: string }> {}
+
+interface ISearchPageState {
+  results: {
+    albums: MusicKit.Resource | null;
+    songs: MusicKit.MediaItem[] | null;
+    playlists: MusicKit.Resource | null;
+    artists: MusicKit.Resource | null;
+  };
+  loading: boolean | null;
+}
+
+class SearchPage extends React.Component<ISearchPageProps, ISearchPageState> {
+  public static playTrack({
+    tracks,
+    index,
+  }: {
+    track: MusicKit.MediaItem;
+    tracks: MusicKit.MediaItem[];
+    index: number;
+  }) {
+    MusicPlayerApi.playTrack(tracks, index);
+  }
+  public readonly ref = React.createRef<HTMLDivElement>();
+
+  constructor(props: ISearchPageProps) {
     super(props);
 
     this.state = {
@@ -33,15 +58,20 @@ class SearchPage extends React.Component {
     this.ref = React.createRef();
   }
 
-  componentDidMount() {
+  public componentDidMount() {
     this.search();
   }
 
-  search = async () => {
+  public search = async () => {
     const query = this.props.match.params.query.replace(' ', '+');
     if (query.length === 0) {
       this.setState({
-        results: null,
+        results: {
+          albums: null,
+          songs: null,
+          playlists: null,
+          artists: null,
+        },
       });
       return;
     }
@@ -66,11 +96,11 @@ class SearchPage extends React.Component {
     });
   };
 
-  searchCatalog = async query => {
-    const res = await this.props.mk.instance.api.search(query, {
+  public searchCatalog = async (query: string) => {
+    const res = (await this.props.mk.instance.api.search(query, {
       types: ['albums', 'songs', 'playlists', 'artists'],
       limit: 24,
-    });
+    })) as any;
     const { results } = this.state;
     this.setState({
       results: {
@@ -83,7 +113,7 @@ class SearchPage extends React.Component {
     });
   };
 
-  searchLibrary = async query => {
+  public searchLibrary = async (query: string) => {
     const res = await this.props.mk.instance.api.library.search(query, {
       types: ['library-albums', 'library-songs', 'library-playlists', 'library-artists'],
       limit: 24,
@@ -100,7 +130,7 @@ class SearchPage extends React.Component {
     });
   };
 
-  renderSongs = () => {
+  public renderSongs = () => {
     const { songs } = this.state.results;
 
     if (!songs || songs.length === 0) {
@@ -110,18 +140,12 @@ class SearchPage extends React.Component {
     return (
       <>
         <h3>{translate.songs}</h3>
-        <TracksGrid
-          scrollElement={this.ref}
-          tracks={songs}
-          showArtist
-          showAlbum
-          playTrack={SearchPage.playTrack}
-        />
+        <TracksGrid tracks={songs} showArtist showAlbum playTrack={SearchPage.playTrack} />
       </>
     );
   };
 
-  renderAlbums = () => {
+  public renderAlbums = () => {
     const { albums } = this.state.results;
 
     if (!albums || albums.length === 0) {
@@ -132,7 +156,7 @@ class SearchPage extends React.Component {
       <>
         <h3>{translate.albums}</h3>
         <div className={classes.searchGrid}>
-          {albums.map(album => (
+          {albums.map((album: MusicKit.Resource) => (
             <AlbumItem key={album.id} album={album} size={120} />
           ))}
         </div>
@@ -140,7 +164,7 @@ class SearchPage extends React.Component {
     );
   };
 
-  renderPlaylists = () => {
+  public renderPlaylists = () => {
     const { playlists } = this.state.results;
 
     if (!playlists || playlists.length === 0) {
@@ -151,7 +175,7 @@ class SearchPage extends React.Component {
       <>
         <h3>{translate.playlists}</h3>
         <div className={classes.searchGrid}>
-          {playlists.map(playlist => (
+          {playlists.map((playlist: MusicKit.Resource) => (
             <PlaylistItem key={playlist.id} playlist={playlist} size={120} />
           ))}
         </div>
@@ -159,7 +183,7 @@ class SearchPage extends React.Component {
     );
   };
 
-  renderArtists = () => {
+  public renderArtists = () => {
     const { artists } = this.state.results;
 
     if (!artists || artists.length === 0) {
@@ -170,7 +194,7 @@ class SearchPage extends React.Component {
       <>
         <h3>{translate.artists}</h3>
         <div className={classes.searchArtistsGrid}>
-          {artists.slice(0, 12).map(artist => (
+          {artists.slice(0, 12).map((artist: MusicKit.Resource) => (
             <ArtistItem artist={artist} size={41} key={artist.id} />
           ))}
         </div>
@@ -178,7 +202,7 @@ class SearchPage extends React.Component {
     );
   };
 
-  renderResults = () => {
+  public renderResults = () => {
     const { loading } = this.state;
 
     const songs = this.renderSongs();
@@ -211,7 +235,7 @@ class SearchPage extends React.Component {
     );
   };
 
-  render() {
+  public render() {
     const { isAuthorized } = this.props.mk.instance;
     const { query } = this.props.match.params;
     return (
@@ -247,16 +271,6 @@ class SearchPage extends React.Component {
       </PageContent>
     );
   }
-
-  static playTrack({ tracks, index }) {
-    MusicPlayerApi.playTrack(tracks, index);
-  }
 }
-
-SearchPage.propTypes = {
-  mk: PropTypes.any.isRequired,
-  query: PropTypes.string.isRequired,
-  match: PropTypes.any.isRequired,
-};
 
 export default withMK(withRouter(SearchPage));
