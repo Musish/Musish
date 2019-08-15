30f70ffd4fd08d18311aff19d833b1fede4823ef
rvigee <contact@raphaelvigee.com>
2019-08-15 11:40:39 +0200

    Move ArtistPage to TS

--- src/app/components/Routes/Catalog/Artist/ArtistPage.tsx
+++ src/app/components/Routes/Catalog/Artist/ArtistPage.tsx
@@ -1,105 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { ReactNode } from 'react';
+import { RouteComponentProps } from 'react-router';
 import { withRouter } from 'react-router-dom';
-import classes from './ArtistPage.scss';
-import PageTitle from '../../../Common/PageTitle/PageTitle';
-import PageContent from '../../../Common/PageContent/PageContent';
-import AlbumItem from '../../../Common/AlbumItem/AlbumItem';
 import backend from '../../../../services/Backend';
 import translate from '../../../../utils/translations/Translations';
+import AlbumItem from '../../../Common/AlbumItem/AlbumItem';
+import PageContent from '../../../Common/PageContent/PageContent';
+import PageTitle from '../../../Common/PageTitle/PageTitle';
 import PlaylistItem from '../../../Common/PlaylistItem/PlaylistItem';
+import classes from './ArtistPage.scss';
 
-class ArtistPage extends React.Component {
-  constructor(props) {
-    super(props);
+interface IArtistPageProps extends RouteComponentProps<{ id: string }> {}
 
-    this.state = {
-      artist: null,
-      geniusData: null,
-    };
+interface IArtistPageState {
+  artist: any;
+  geniusData: any;
+  albums: any;
+  playlists: any;
+}
 
-    this.pageRef = React.createRef();
-  }
-
-  componentDidMount() {
-    this.refreshData();
-  }
-
-  componentDidUpdate() {
-    if (this.state.artist && this.state.artist.id !== this.props.match.params.id) {
-      this.refreshData();
-    }
-  }
-
-  fetchArtist = async () => {
-    const music = MusicKit.getInstance();
-
-    const { id } = this.props.match.params;
-    const isCatalog = /^\d+$/.test(id);
-
-    let artist;
-    if (isCatalog) {
-      artist = await music.api.artist(id);
-    } else {
-      artist = await music.api.library.artist(id);
-    }
-
-    this.setState({
-      artist,
-    });
-  };
-
-  fetchAlbums = async () => {
-    const music = MusicKit.getInstance();
-
-    const { id } = this.props.match.params;
-    const isCatalog = /^\d+$/.test(id);
-
-    let albums;
-    if (isCatalog) {
-      albums = await music.api.artist(id, { include: 'albums' });
-    } else {
-      albums = await music.api.library.artist(id, { include: 'albums' });
-    }
-
-    this.setState({
-      albums,
-    });
-  };
-
-  fetchPlaylists = async () => {
-    const music = MusicKit.getInstance();
-
-    const { id } = this.props.match.params;
-    const isCatalog = /^\d+$/.test(id);
-
-    let playlists;
-    if (isCatalog) {
-      playlists = await music.api.artist(id, { include: 'playlists' });
-    }
-
-    this.setState({
-      playlists,
-    });
-  };
-
-  fetchGeniusData = async () => {
-    const { id } = this.props.match.params;
-
-    const isCatalog = /^\d+$/.test(id);
-    if (!isCatalog) {
-      return;
-    }
-
-    const { data } = await backend.get(`/genius/artist?artistId=${id}`);
-    data.plainDescription = ArtistPage.flattenDesc(data.description.dom.children);
-
-    this.setState({
-      geniusData: data,
-    });
-  };
-
-  static flattenDesc(object, props = {}) {
+class ArtistPage extends React.Component<IArtistPageProps, IArtistPageState> {
+  public static flattenDesc(object: any, props = {}): ReactNode {
     if (typeof object === 'string') {
       return object;
     }
@@ -120,14 +40,105 @@ class ArtistPage extends React.Component {
     );
   }
 
-  refreshData = () => {
+  public readonly pageRef = React.createRef<HTMLDivElement>();
+
+  constructor(props: IArtistPageProps) {
+    super(props);
+
+    this.state = {
+      artist: null,
+      albums: null,
+      playlists: null,
+      geniusData: null,
+    };
+  }
+
+  public componentDidMount() {
+    this.refreshData();
+  }
+
+  public componentDidUpdate() {
+    if (this.state.artist && this.state.artist.id !== this.props.match.params.id) {
+      this.refreshData();
+    }
+  }
+
+  public fetchArtist = async () => {
+    const music = MusicKit.getInstance();
+
+    const { id } = this.props.match.params;
+    const isCatalog = /^\d+$/.test(id);
+
+    let artist;
+    if (isCatalog) {
+      artist = await music.api.artist(id);
+    } else {
+      artist = await music.api.library.artist(id);
+    }
+
+    this.setState({
+      artist,
+    });
+  };
+
+  public fetchAlbums = async () => {
+    const music = MusicKit.getInstance();
+
+    const { id } = this.props.match.params;
+    const isCatalog = /^\d+$/.test(id);
+
+    let albums;
+    if (isCatalog) {
+      albums = await music.api.artist(id, { include: 'albums' });
+    } else {
+      albums = await music.api.library.artist(id, { include: 'albums' });
+    }
+
+    this.setState({
+      albums,
+    });
+  };
+
+  public fetchPlaylists = async () => {
+    const music = MusicKit.getInstance();
+
+    const { id } = this.props.match.params;
+    const isCatalog = /^\d+$/.test(id);
+
+    let playlists;
+    if (isCatalog) {
+      playlists = await music.api.artist(id, { include: 'playlists' });
+    }
+
+    this.setState({
+      playlists,
+    });
+  };
+
+  public fetchGeniusData = async () => {
+    const { id } = this.props.match.params;
+
+    const isCatalog = /^\d+$/.test(id);
+    if (!isCatalog) {
+      return;
+    }
+
+    const { data } = await backend.get(`/genius/artist?artistId=${id}`);
+    data.plainDescription = ArtistPage.flattenDesc(data.description.dom.children);
+
+    this.setState({
+      geniusData: data,
+    });
+  };
+
+  public refreshData = () => {
     this.fetchArtist();
     this.fetchAlbums();
     this.fetchPlaylists();
     this.fetchGeniusData();
   };
 
-  render() {
+  public render() {
     const { artist, albums, playlists, geniusData } = this.state;
 
     const headerStyles = {
@@ -160,7 +171,7 @@ class ArtistPage extends React.Component {
             <h3>{translate.albums}</h3>
 
             <div className={classes.albumsGrid}>
-              {albums.map(album => (
+              {albums.map((album: MusicKit.Resource) => (
                 <AlbumItem key={album.id} album={album} size={120} />
               ))}
             </div>
@@ -172,7 +183,7 @@ class ArtistPage extends React.Component {
             <h3>{translate.playlists}</h3>
 
             <div className={classes.playlistsGrid}>
-              {playlists.map(playlist => (
+              {playlists.map((playlist: MusicKit.Resource) => (
                 <PlaylistItem key={playlist.id} playlist={playlist} size={120} />
               ))}
             </div>
@@ -182,9 +193,5 @@ class ArtistPage extends React.Component {
     );
   }
 }
-
-ArtistPage.propTypes = {
-  match: PropTypes.any.isRequired,
-};
 
 export default withRouter(ArtistPage);
